a6446c0ef5d971dae17ca4f50bc0d69fb646b7db
blpoke <84424526+blpoke@users.noreply.github.com>
2025-05-26 11:47:58 +1000

    hotspot carousel

--- apps/web/src/components/sections/HotspotImageCarousel.tsx
+++ apps/web/src/components/sections/HotspotImageCarousel.tsx
@@ -12,8 +12,9 @@ import { AnimatePresence, motion, Variants } from "motion/react";
 import { twMerge } from "tailwind-merge";
 import Link from "next/link";
 import { useIsMobile } from "@/hooks/use-is-mobile";
-import { sectionAnimationConfig } from "@/lib/motion";
+import { customEase, sectionAnimationConfig } from "@/lib/motion";
 import SectionHeader from "../section-header";
+import { cn } from "@/lib/utils";
 
 const SLIDE_VARIANTS = {
   initial: { opacity: 0 },
@@ -23,27 +24,52 @@ const SLIDE_VARIANTS = {
 
 function SlideProgressBar({
   currentIndex,
+  activeHotspot,
   setCurrentIndex,
   images,
+  className,
+  varyColour = false,
 }: {
   currentIndex: number;
+  activeHotspot?: number | null;
   setCurrentIndex: (index: number) => void;
   images: ImageWithHotspotProps[];
+  className?: string;
+  varyColour?: boolean;
 }) {
   return (
-    <div className="absolute bottom-0 right-0 flex gap-2 p-4">
+    <div className={cn("flex justify-center gap-2 p-4", className)}>
       {images.map(({ _key }, index) => (
         <div
           key={`timer-${_key}-${index}`}
           onClick={() => {
             setCurrentIndex(index);
           }}
-          className="h-1 w-[82px] flex-1 rounded-full overflow-hidden bg-white/20 cursor-pointer"
+          className={cn(
+            "h-1 w-10 lg:w-[82px] rounded-full overflow-hidden bg-white/20 cursor-pointer transition-colors duration-500",
+            varyColour && activeHotspot !== null
+              ? "bg-background/20"
+              : "bg-text/20",
+          )}
         >
-          {currentIndex > index && <div className="h-full w-full bg-white" />}
+          {currentIndex > index && (
+            <div
+              className={cn(
+                "h-full w-full bg-white transition-colors duration-500",
+                varyColour && activeHotspot !== null
+                  ? "bg-background"
+                  : "bg-text",
+              )}
+            />
+          )}
           {currentIndex === index && (
             <motion.div
-              className="h-full bg-white"
+              className={cn(
+                "h-full bg-white transition-colors duration-500",
+                varyColour && activeHotspot !== null
+                  ? "bg-background"
+                  : "bg-text",
+              )}
               initial={{ width: "0%" }}
               animate={{ width: "100%" }}
               transition={{ duration: 10, ease: "linear" }}
@@ -84,7 +110,7 @@ function SlideBase({
                 setActiveHotspot(activeHotspot === index ? null : index)
               }
               className={twMerge(
-                "absolute z-10 lg:w-12 lg:h-12 w-6 h-6 flex items-center justify-center rounded-full cursor-pointer border",
+                "absolute z-10 lg:w-12 lg:h-12 w-6 h-6 flex items-center justify-center rounded-full cursor-pointer border transition-colors duration-500",
                 activeHotspot === index ? "border-white" : "border-transparent",
               )}
               style={{
@@ -237,6 +263,7 @@ function MobileHotspotImageCarousel({
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prev) => (prev + 1) % images.length);
+      setActiveHotspot(0);
     }, 10000);
     return () => clearInterval(interval);
   }, [images, currentIndex]);
@@ -244,117 +271,131 @@ function MobileHotspotImageCarousel({
   return (
     <motion.div {...sectionAnimationConfig} className="wrapper">
       <SectionHeader {...sectionHeader} className="my-fluid-xs" />
-      <div className="relative aspect-video rounded-xl overflow-hidden mt-fluid-xs">
-        <AnimatePresence mode="sync">
-          {images.map(
-            (item, index) =>
-              currentIndex === index && (
-                <motion.div
-                  variants={SLIDE_VARIANTS}
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  key={item._key}
-                  className="absolute inset-0"
-                >
-                  <SlideBase
-                    {...item}
-                    setActiveHotspot={setActiveHotspot}
-                    activeHotspot={activeHotspot}
-                  />
-                </motion.div>
-              ),
+      <div className="rounded-xl overflow-hidden mt-fluid-xs">
+        <div
+          className={cn(
+            "aspect-video relative overflow-hidden transition-all duration-500",
+            activeHotspot === null && "rounded-xl",
           )}
-        </AnimatePresence>
-        <SlideProgressBar
-          currentIndex={currentIndex}
-          setCurrentIndex={setCurrentIndex}
-          images={images}
-        />
+        >
+          <AnimatePresence>
+            {images.map(
+              (item, index) =>
+                currentIndex === index && (
+                  <motion.div
+                    variants={SLIDE_VARIANTS}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    key={item._key}
+                    className="absolute inset-0"
+                  >
+                    <SlideBase
+                      {...item}
+                      setActiveHotspot={setActiveHotspot}
+                      activeHotspot={activeHotspot}
+                    />
+                  </motion.div>
+                ),
+            )}
+          </AnimatePresence>
+        </div>
+
+        <div
+          className={cn(
+            "transition-colors duration-500",
+            activeHotspot !== null
+              ? "bg-text text-background"
+              : "bg-background text-text",
+          )}
+        >
+          <SlideProgressBar
+            currentIndex={currentIndex}
+            setCurrentIndex={setCurrentIndex}
+            images={images}
+            varyColour
+            activeHotspot={activeHotspot}
+          />
+
+          <AnimatePresence mode="wait">
+            {images[currentIndex].hotspots.map(
+              (spot, index) =>
+                activeHotspot === index && (
+                  <motion.div
+                    key={`hotspot-${spot._key}-${index}`}
+                    initial={{ height: 0 }}
+                    animate={{ height: "auto" }}
+                    exit={{ height: 0 }}
+                    transition={{ duration: 0.75, ease: customEase }}
+                  >
+                    <div className="p-4 pt-12 space-y-8">
+                      <div className="flex w-full justify-between items-center gap-2">
+                        <p className="text-xs">Solutions</p>
+                        <div className="flex gap-2">
+                          <button
+                            onClick={() =>
+                              setActiveHotspot(
+                                (index -
+                                  1 +
+                                  images[currentIndex].hotspots.length) %
+                                  images[currentIndex].hotspots.length,
+                              )
+                            }
+                            className="bg-background rounded-full flex w-10 h-10 items-center justify-center text-text"
+                          >
+                            <svg
+                              width="5"
+                              height="10"
+                              viewBox="0 0 5 10"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M4.5 9L0.593741 5.09374L4.5 1.18748"
+                                stroke="currentColor"
+                                strokeWidth="0.781252"
+                              />
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() =>
+                              setActiveHotspot(
+                                (index + 1) %
+                                  images[currentIndex].hotspots.length,
+                              )
+                            }
+                            className="bg-background rounded-full flex w-10 h-10 items-center justify-center text-text"
+                          >
+                            <svg
+                              width="6"
+                              height="10"
+                              viewBox="0 0 6 10"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M1.09375 1L5.00001 4.90626L1.09375 8.81252"
+                                stroke="currentColor"
+                                strokeWidth="0.781252"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                      <Link
+                        href={spot.solution.slug ?? ""}
+                        className="underline block"
+                      >
+                        {spot.solution.title}
+                      </Link>
+                      <p className=" text-sm mb-4">{spot.description}</p>
+                    </div>
+                  </motion.div>
+                ),
+            )}
+          </AnimatePresence>
+        </div>
       </div>
-      {images[currentIndex].hotspots.map(
-        (spot, index) =>
-          activeHotspot === index && (
-            <motion.div
-              key={`hotspot-${spot._key}-${index}`}
-              className="bg-nav-bar-background/20 rounded-lg backdrop-blur-lg p-4 mt-fluid-xs text-text"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              <div className="w-full aspect-video overflow-hidden rounded-lg relative">
-                <SanityImage
-                  src={images[currentIndex].image}
-                  alt={images[currentIndex].image.alt ?? ""}
-                  className="w-full h-full object-cover"
-                  style={{
-                    transform: `scale(5)`,
-                    transformOrigin: `${spot.x}% ${spot.y}%`,
-                    transition: "transform 0.3s ease-out",
-                  }}
-                />
-              </div>
-              <div className="p-2 space-y-8">
-                <div className="flex w-full justify-between gap-2">
-                  <p className="text-xs">Solutions</p>
-                  <div className="flex gap-2">
-                    <button
-                      onClick={() =>
-                        setActiveHotspot(
-                          (index - 1 + images[currentIndex].hotspots.length) %
-                            images[currentIndex].hotspots.length,
-                        )
-                      }
-                    >
-                      <svg
-                        width="5"
-                        height="10"
-                        viewBox="0 0 5 10"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M4.5 9L0.593741 5.09374L4.5 1.18748"
-                          stroke="currentColor"
-                          strokeWidth="0.781252"
-                        />
-                      </svg>
-                    </button>
-                    <button
-                      onClick={() =>
-                        setActiveHotspot(
-                          (index + 1) % images[currentIndex].hotspots.length,
-                        )
-                      }
-                    >
-                      <svg
-                        width="6"
-                        height="10"
-                        viewBox="0 0 6 10"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M1.09375 1L5.00001 4.90626L1.09375 8.81252"
-                          stroke="currentColor"
-                          strokeWidth="0.781252"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <Link
-                  href={spot.solution.slug ?? ""}
-                  className=" underline block"
-                >
-                  {spot.solution.title}
-                </Link>
-                <p className=" text-sm mb-4">{spot.description}</p>
-              </div>
-            </motion.div>
-          ),
-      )}
     </motion.div>
   );
 }
@@ -404,6 +445,7 @@ function DesktopHotspotImageCarousel({
           currentIndex={currentIndex}
           setCurrentIndex={setCurrentIndex}
           images={images}
+          className="absolute bottom-0 right-0 "
         />
       </div>
     </motion.div>
@@ -415,11 +457,6 @@ export default function HotspotImageCarousel({
   sectionHeader,
 }: ImageWithProductHotspotsProps) {
   const isMobile = useIsMobile();
-
-  // Return null on initial render to avoid hydration mismatch
-  if (isMobile === undefined) {
-    return null;
-  }
 
   return (
     <>
