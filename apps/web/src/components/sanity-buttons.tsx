aa9fb0c5fab66db7c15a7b996e23cc0cbcd284ea
blpoke <84424526+blpoke@users.noreply.github.com>
2025-05-30 10:56:25 +1000

    Update sanity-buttons.tsx

--- apps/web/src/components/sanity-buttons.tsx
+++ apps/web/src/components/sanity-buttons.tsx
@@ -3,6 +3,8 @@ import { TypeFromSelection } from "groqd";
 import { twMerge } from "tailwind-merge";
 import { Button } from "./ui/Button";
 import Link from "next/link";
+import PageAnimation from "./PageAnimation";
+import { useTransitionRouter } from "next-view-transitions";
 
 type SanityButtonsProps = {
   buttons: SanityButtonProps[] | null;
@@ -21,11 +23,19 @@ export function SanityButtons({
 }: SanityButtonsProps) {
   if (!buttons?.length) return null;
 
+  const router = useTransitionRouter();
+
   return (
     <div className={twMerge("flex flex-col sm:flex-row gap-4", className)}>
       {buttons.map(({ url, variant, text, _key }, i) => (
         <Link
           key={_key}
+          onClick={(e) => {
+            e.preventDefault();
+            router.push((url?.href as string) ?? "/", {
+              onTransitionReady: PageAnimation,
+            });
+          }}
           href={url?.href ?? "#"}
           target={url?.openInNewTab ? "_blank" : "_self"}
         >
